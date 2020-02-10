476645d8420c2440578a18e5f1172ce52d613b1a
Raphael <raphael@fabco.tech>
2020-02-10 15:23:35 +0100

    - compat with rnode 9.19, dappy-cli 0.2.8

--- script_2.js
+++ script_2.js
@@ -1,16 +1,18 @@
 const loadUnforgeableName = () => {
-  dappyRChain.fetch('dappy://rchain/alphanetwork/UNFORGEABLE_NAME_1')
-      .then(a => {
-        const rholangTerm = JSON.parse(a);
-        const jsObject = blockchainUtils.rhoValToJs(rholangTerm);
-        registryUri = jsObject.registry_uri;
-        lightUnforgeableName = jsObject.unforgeable_name_light[0].gPrivate;
-        console.log('lightUnforgeableName is ', lightUnforgeableName);
-        console.log('registryUri is ', registryUri);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  dappyRChain
+    .fetch("dappy://rchain/alphanetwork/UNFORGEABLE_NAME_1")
+    .then(a => {
+      const response = JSON.parse(a);
+      const rholangTerm = response.expr;
+      const jsObject = blockchainUtils.rhoValToJs(rholangTerm);
+      registryUri = jsObject.registry_uri;
+      lightUnforgeableName = jsObject.unforgeable_name_light.UnforgPrivate;
+      console.log("lightUnforgeableName is ", lightUnforgeableName);
+      console.log("registryUri is ", registryUri);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 loadUnforgeableName();
